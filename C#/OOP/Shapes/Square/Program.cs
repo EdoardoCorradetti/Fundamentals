397ab0244943f730482a24a5fe723a6ef333d758
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-04-07 17:30:54 +0200

    New projects

--- C#/OOP/Shapes/Square/Program.cs
+++ C#/OOP/Shapes/Square/Program.cs
@@ -6,7 +6,14 @@ namespace Square
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Square s = new Square(); //instance constructor
+            Console.Write("Type the square's side: ");
+            s.side = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The square's perimeter is: " + s.perimeter());
+            Console.WriteLine("The square's area is: " + s.area());
+            Console.WriteLine("The square's diagonal is: " + s.diagonal());
+            Console.ReadKey();
         }
     }
 }
---
+++ C#/OOP/Shapes/Square/Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square
+{
+    class Square
+    {
+        public double side;         //side is an attribute, public is an access modifier, you can read it or write it in another assembly
+        public double perimeter()   //perimeter is a method, no void after public so you need to return something
+        {
+            double p = side * 4;
+            return p;
+        }
+        public double area()
+        {
+            double a = side * side;
+            return a;
+        }
+
+        public double diagonal()
+        {
+            double d = side * Math.Sqrt(2);
+            return d;
+        }
+    }
+}
