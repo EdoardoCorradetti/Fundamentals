e78ed12a61e9c4a5ee6e20e964e9316607b4156d
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-04-07 17:52:24 +0200

    Update Square.cs

--- C#/OOP/Shapes/Square/Square.cs
+++ C#/OOP/Shapes/Square/Square.cs
@@ -7,18 +7,18 @@ namespace Square
     class Square
     {
         public double side;         //side is an attribute, public is an access modifier, you can read it or write it in another assembly
-        public double perimeter()   //perimeter is a method, no void after public so you need to return something
+        public double Perimeter()   //perimeter is a method, no void after public so you need to return something
         {
             double p = side * 4;
             return p;
         }
-        public double area()
+        public double Area()
         {
             double a = side * side;
             return a;
         }
 
-        public double diagonal()
+        public double Diagonal()
         {
             double d = side * Math.Sqrt(2);
             return d;
