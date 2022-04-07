7e95333111b08ed8b19c0884dc608ddcce450ba8
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-04-07 17:52:10 +0200

    Create Circle.cs

---
+++ C#/OOP/Shapes/Circle/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle
+{
+    class Circle
+    {
+        private double radius; //private is an access modifier, you can't read it or write it in another assembly (it's default in a class)
+        public double Diameter()
+        
+    }
+}
